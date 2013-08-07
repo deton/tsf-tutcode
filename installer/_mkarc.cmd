63ea429d876e915fff86da19dc5dce91ab9a72e1
KIHARA Hideto <deton@m1.interq.or.jp>
2013-08-07 12:26:50 +0900

    config-sample.7zのアーカイブ処理を削除。 config-sample/kanatable.txtを削除して空になったので。

--- installer/_mkarc.cmd
+++ installer/_mkarc.cmd
@@ -2,11 +2,6 @@
 
 pushd %~dp0
 
-pushd config-sample
-7za.exe a config-sample.7z *
-move config-sample.7z ..\
-popd
-
-7za.exe a tsftutcode.7z tsftutcode-x64.msi tsftutcode-x86.msi ..\README.TXT ..\LICENSE.TXT tuttable.txt maze.dic config-sample.7z
+7z.exe a tsftutcode.7z tsftutcode-x64.msi tsftutcode-x86.msi ..\README.TXT ..\LICENSE.TXT tutcode.tbl tcode.tbl maze.dic
 
 popd
