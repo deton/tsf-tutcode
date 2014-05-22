72ef698f6e48a1e114ab8dd082901a160a2f4f6b
KIHARA Hideto <deton@m1.interq.or.jp>
2014-05-22 20:49:25 +0900

    Rename zip file name from tsftutcode.zip to tsf-tutcode.zip.

--- installer/_mkarc.cmd
+++ installer/_mkarc.cmd
@@ -11,7 +11,7 @@ pushd config-lua
 popd
 
 pushd %TARGETDIR%
-7z.exe a -tzip tsftutcode.zip tsftutcode-x64.msi tsftutcode-x86.msi ..\..\README.TXT ..\..\LICENSE.TXT ..\tutcode.tbl ..\tcode.tbl ..\maze.dic config-lua.zip
+7z.exe a -tzip tsf-tutcode.zip tsftutcode-x64.msi tsftutcode-x86.msi ..\..\README.TXT ..\..\LICENSE.TXT ..\tutcode.tbl ..\tcode.tbl ..\maze.dic config-lua.zip
 popd
 
 popd
