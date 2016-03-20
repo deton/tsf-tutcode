4a99b5d46a3ea12f7632a6591f88a329a8e4e732
KIHARA Hideto <deton@m1.interq.or.jp>
2016-03-20 19:25:41 +0900

    inst32.batでinit.luaのコピーも追加
    でないと、関数の引数追加変更等の際にうまく動かなくなるので。

--- inst32.bat
+++ inst32.bat
@@ -1,3 +1,4 @@
 taskkill /IM imtutmgr.exe
 copy /Y win32\Release\*.dll \Windows\System32\IME\IMTSFTUTCODE
 copy /Y win32\Release\*.exe \Windows\System32\IME\IMTSFTUTCODE
+copy /Y installer\config-lua\init.lua \Windows\System32\IME\IMTSFTUTCODE
